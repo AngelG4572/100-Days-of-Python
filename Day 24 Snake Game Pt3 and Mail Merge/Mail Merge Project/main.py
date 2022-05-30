709c91abed415297c94c3a368b5a8151e7468385
AngelG4572 <76978329+AngelG4572@users.noreply.github.com>
2022-05-30 03:59:43 -0600

    Completed Day 24

--- Day 24 Snake Game Pt3 and Mail Merge/Mail Merge Project/main.py
+++ Day 24 Snake Game Pt3 and Mail Merge/Mail Merge Project/main.py
@@ -1,9 +1,9 @@
-with open("../Mail Merge Project/Input/Names/invited_names.txt") as data:
+with open("./Input/Names/invited_names.txt") as data:
     unedited_names = data.readlines()
     for txt in unedited_names:
         new_name = txt.strip("\n")
 
-        with open(f"../Mail Merge Project/Output/ReadyToSend/letter_for_{new_name}", mode="w") as letter:
-            contents = open("../Mail Merge Project/Input/Letters/starting_letter.txt").read()
+        with open(f"./Output/ReadyToSend/letter_for_{new_name}", mode="w") as letter:
+            contents = open("./Input/Letters/starting_letter.txt").read()
             new_content = contents.replace("[name]", new_name)
             letter.write(new_content)
