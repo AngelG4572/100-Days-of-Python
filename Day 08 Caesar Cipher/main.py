dd117e94ac57b37577d6641ca3e2e6536e8b8ffe
Angelina <76978329+AngelG4572@users.noreply.github.com>
2023-10-18 19:01:32 -0700

    Day 08

--- Day 08 Caesar Cipher/main.py
+++ Day 08 Caesar Cipher/main.py
@@ -6,7 +6,7 @@ print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 running = True
-while running == True: 
+while running:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n")) % 26
